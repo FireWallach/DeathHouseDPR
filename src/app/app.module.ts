4d94ef308fcf1e8614aa625a6a45dae88ec18135
Dylan Wallach <dylanwallach80@gmail.com>
2022-06-11 18:10:25 -0400

    Theme and grid changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,14 +6,7 @@ import { AppComponent } from './app.component';
 import { DPRCalcComponent } from './dprcalc/dprcalc.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { DprToolbarComponent } from './dpr-toolbar/dpr-toolbar.component';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatToolbarHarness } from '@angular/material/toolbar/testing';
-import { MatIconModule } from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input';
-import {MatButtonToggleModule} from '@angular/material/button-toggle';
-import {MatGridListModule} from '@angular/material/grid-list';
+import { MaterialModule } from './material/material.module';
 
 
 
@@ -23,15 +16,9 @@ import {MatGridListModule} from '@angular/material/grid-list';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatToolbarModule,
-    MatIconModule,
-    MatButtonModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatButtonToggleModule,
-    MatGridListModule
+    MaterialModule
   ],
-  providers: [MatToolbarHarness],
+  providers: [],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
