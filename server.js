ac2eacf027d7adc12ccc00da2670b018bf2629cc
Dylan Wallach <dylanwallach80@gmail.com>
2022-06-11 15:38:37 -0400

    Config Changes for Heroku Deployment

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const port = 8080;
+
+function requireHTTPS(req, res, next) {
+    // The 'x-forwarded-proto' check is for Heroku
+    if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
+        return res.redirect('https://' + req.get('host') + req.url);
+    }
+    next();
+}
+
+app.use('/', express.static('dist/dprcalc'));
+app.use(requireHTTPS);
+app.get('/*', function(req, res) {
+  res.sendFile('index.html', {root: 'dist/<name-on-package.json>/'}
+);
+});
+app.listen(process.env.PORT || port, () => console.log(`Example app listening on port ${process.env.PORT || port}!`));
